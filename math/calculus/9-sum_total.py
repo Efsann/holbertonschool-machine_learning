aef548e98744919db94ada03e9908280eec77982
Efsann <stiyevefsan@gmail.com>
2026-04-19 18:10:12 +0000

    Fix pycodestyle trailing whitespace errors

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -8,12 +8,9 @@ def summation_i_squared(n):
     """
     Calculates the sum of squares from 1 to n.
     """
-    # N-in etibarlı rəqəm (tam və ya kəsr kimsiz) olub-olmadığını yoxlayırıq
     if type(n) not in (int, float) or int(n) != n or n < 1:
         return None
-    
-    n = int(n)
-    
-    # Riyazi düsturla hesablayıb nəticəni tam rəqəm (integer) kimi qaytarırıq
-    return (n * (n + 1) * (2 * n + 1)) // 6
 
+    n = int(n)
+
+    return (n * (n + 1) * (2 * n + 1)) // 6
