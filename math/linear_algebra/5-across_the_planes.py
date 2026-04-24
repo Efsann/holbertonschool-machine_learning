ccb19f729996187cf6ebbc476dfb687e07b67f09
efsann <efsan@example.com>
2026-04-24 18:15:29 +0000

    Task 5: 2D matrix addition

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Adds two 2D matrices element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """Adds two 2D matrices and returns a new matrix"""
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+
+    new_matrix = []
+    for i in range(len(mat1)):
+        row = [mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+        new_matrix.append(row)
+    return new_matrix
