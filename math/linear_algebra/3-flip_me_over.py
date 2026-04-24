139652e8a7c7feef8f52b1e2d66e2caf9860e5c1
efsann <efsan@example.com>
2026-04-24 18:13:31 +0000

    Fix pycodestyle: line too long

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,4 +4,5 @@
 
 def matrix_transpose(matrix):
     """Calculates the transpose of a 2D matrix"""
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    return [[matrix[j][i] for j in range(len(matrix))]
+            for i in range(len(matrix[0]))]
