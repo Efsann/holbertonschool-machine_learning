559d61f97927e1565c66192cbfba9a98be2b6c65
efsann <efsan@example.com>
2026-04-24 18:24:31 +0000

    Fix pycodestyle: E501 line too long in task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -4,5 +4,5 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """Returns a new numpy.ndarray that is the concatenation of mat1 and mat2"""
+    """Concatenates two matrices along a specific axis"""
     return np.concatenate((mat1, mat2), axis=axis)
