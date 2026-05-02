255c6d82d2e4e92fa8a1bcc20e983456779f5af1
efsann <stiyevefsan@gmail.com>
2026-05-02 16:46:44 +0000

    Task 0: Calculate determinant from scratch

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module to calculate the determinant of a matrix"""
+
+
+def determinant(matrix):
+    """
+    Calculates the determinant of a matrix.
+    Uses recursion for matrices larger than 2x2.
+    """
+    if (not isinstance(matrix, list) or len(matrix) == 0 or
+            not all(isinstance(row, list) for row in matrix)):
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
+        return 1
+
+    n = len(matrix)
+    for row in matrix:
+        if len(row) != n:
+            raise ValueError("matrix must be a square matrix")
+
+    if n == 1:
+        return matrix[0][0]
+    
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for c in range(n):
+        minor = [row[:c] + row[c+1:] for row in matrix[1:]]
+        det += ((-1) ** c) * matrix[0][c] * determinant(minor)
+
+    return det
