5933487aefcffe0ce49d55936d1f76e809717354
efsann <stiyevefsan@gmail.com>
2026-05-14 17:37:45 +0000

    Task 2: Add cdf method to Poisson class

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -47,3 +47,23 @@ class Poisson:
         pmf_value = (self.lambtha ** k) * (e ** -self.lambtha) / factorial
 
         return pmf_value
+
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of successes.
+
+        Args:
+            k: the number of successes
+
+        Returns:
+            The CDF value for k, or 0 if k is out of range.
+        """
+        k = int(k)
+        if k < 0:
+            return 0
+
+        cdf_value = 0
+        for i in range(k + 1):
+            cdf_value += self.pmf(i)
+
+        return cdf_value
