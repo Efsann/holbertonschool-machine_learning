d02504d41705a466eecf39c80a880f1e1410e698
efsann <stiyevefsan@gmail.com>
2026-05-14 17:40:23 +0000

    Task 5: Add cdf method to Exponential class

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -42,3 +42,21 @@ class Exponential:
         pdf_value = self.lambtha * (e ** (-self.lambtha * x))
 
         return pdf_value
+
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given time period.
+
+        Args:
+            x: the time period
+
+        Returns:
+            The CDF value for x, or 0 if x is out of range.
+        """
+        if x < 0:
+            return 0
+
+        e = 2.7182818285
+        cdf_value = 1 - (e ** (-self.lambtha * x))
+
+        return cdf_value
