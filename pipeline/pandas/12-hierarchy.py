2c23464f2f7a65cc9cdd71b9204a43aae6fd505f
efsann <efsann@github.com>
2026-05-25 20:38:50 +0000

    Fix: Remove blank lines to resolve W293 pycodestyle error in hierarchy

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -10,17 +10,9 @@ def hierarchy(df1, df2):
     """
     Concatenates two DataFrames, swaps index levels and sorts chronologically
     """
-    # 1. Cədvəlləri indeksləyir və göstərilən aralıqda kəsirik
     df1 = index(df1).loc[1417411980:1417417980]
     df2 = index(df2).loc[1417411980:1417417980]
-    
-    # 2. Cədvəlləri alt-alta birləşdirir və açar sözlər təyin edirik
     df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-    
-    # 3. İndeks səviyyələrinin yerini dəyişirik (Timestamp birinci olur)
     df = df.swaplevel(0, 1)
-    
-    # 4. İndeksə (xronoloji ardıcıllığa) görə çeşidləyirik
     df = df.sort_index()
-    
     return df
