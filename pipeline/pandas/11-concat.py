d3a68dbbf23e178d5649cf1f1ebd7548378025e3
efsann <efsann@github.com>
2026-05-25 20:37:40 +0000

    Fix: Remove blank lines to resolve W293 pycodestyle error

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -10,14 +10,7 @@ def concat(df1, df2):
     """
     Indexes both dataframes, filters df2, and concatenates them
     """
-    # 1. Hər iki cədvəlin Timestamp sütununu indeks edirik
     df1 = index(df1)
     df2 = index(df2)
-    
-    # 2. df2 cədvəlini 1417411920 daxil olmaqla kəsirik
     df2 = df2.loc[:1417411920]
-    
-    # 3. df2 üstə, df1 isə altda olmaqla birləşdirib açar sözlər təyin edirik
-    result = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-    
-    return result
+    return pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
