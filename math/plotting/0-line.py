0c8577484aed08569092531307b4f689cef29bb7
efsann <stiyevefsan@gmai.com>
2026-05-26 18:26:23 +0000

    Task 0: Moved plotting files to the correct math/plotting directory

---
+++ math/plotting/0-line.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Line graph module
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def line():
+    """
+    Plots y as a solid red line graph
+    """
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(y, 'r-')
+    plt.xlim(0, 10)
+    plt.show()
