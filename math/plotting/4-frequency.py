288080c94f6b5d880911897856964e01a188c55a
efsann <stiyevefsan@gmai.com>
2026-05-26 18:32:34 +0000

    Fix: Completely remove whitespaces and fix pycodestyle W293

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,8 +13,6 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    
-    # Bins və kənar xətlər tam standartlaşdırılır
     plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
     plt.xlim(0, 100)
     plt.ylim(0, 30)
