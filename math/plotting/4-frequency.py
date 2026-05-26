3062a8d258ec0e7b9b948ab3d27b4fd375a24116
efsann <stiyevefsan@gmai.com>
2026-05-26 18:31:54 +0000

    Fix: Adjust frequency plot to match reference exactly and cleanup main files

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -14,9 +14,10 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
     
-    # Referans qrafiklə tam üst-üstə düşməsi üçün:
+    # Bins və kənar xətlər tam standartlaşdırılır
     plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
     plt.xlim(0, 100)
+    plt.ylim(0, 30)
     plt.xticks(np.arange(0, 101, 10))
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
