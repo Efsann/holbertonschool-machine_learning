9f881e68d75b0d7eefbe4095758ce0f3c5a087eb
Stiyeve Fsan <stiyevefsan@gmail.com>
2026-06-28 14:24:16 +0000

    Task 2: Precision function implemented separately

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -10,6 +10,11 @@ def precision(confusion):
     confusion: (classes, classes) ölçülü confusion matrix
     Returns: hər sinif üçün precision dəyərlərini saxlayan (classes,) massivi
     """
+    # Diaqonaldakı True Positive-ləri götürürük
     tp = np.diag(confusion)
+    
+    # Sütun üzrə cəmləri (True Positives + False Positives) tapırıq
     predicted_total = np.sum(confusion, axis=0)
+    
+    # Precision = TP / (TP + FP)
     return tp / predicted_total
