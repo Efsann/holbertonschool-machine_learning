a1a43c94cfea7a93465e88b0094c0ef8ed9d6680
efsann <stiyevefsan@gmail.com>
2026-07-17 19:45:32 +0000

    Implement save and load configuration

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Module containing functions to save and load Keras model configurations
+"""
+import tensorflow as tf
+
+
+def save_config(network, filename):
+    """
+    Saves a model's configuration in JSON format.
+
+    Parameters:
+    network: The model whose configuration should be saved.
+    filename: The path of the file that the configuration should be saved to.
+
+    Returns:
+    None
+    """
+    json_config = network.to_json()
+    with open(filename, 'w') as f:
+        f.write(json_config)
+
+
+def load_config(filename):
+    """
+    Loads a model with a specific configuration.
+
+    Parameters:
+    filename: The path of the file containing the model's configuration
+              in JSON format.
+
+    Returns:
+    The loaded model.
+    """
+    with open(filename, 'r') as f:
+        json_config = f.read()
+    return tf.keras.models.model_from_json(json_config)
