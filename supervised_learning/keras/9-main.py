4e27d2c6fd6cec27685c3b387650b47309af0e01
efsann <stiyevefsan@gmail.com>
2026-07-17 19:41:12 +0000

    Add save_model and load_model functions

--- supervised_learning/keras/9-main.py
+++ supervised_learning/keras/9-main.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python3
-import tensorflow as tf
+"""
+Module to save and load Keras models
+"""
+import tensorflow.keras as K
 
-# --- SƏNİN YAZMALI OLDUĞUN FUNKSİYALAR ---
 
 def save_model(network, filename):
-    """Modeli tam olaraq fayla yazır"""
+    """
+    Saves an entire Keras model to a file.
+
+    Args:
+        network: The Keras model to be saved.
+        filename: The path of the file where the model should be saved.
+
+    Returns:
+        None
+    """
     network.save(filename)
 
+
 def load_model(filename):
-    """Modeli fayldan geri yükləyir"""
-    return tf.keras.models.load_model(filename)
+    """
+    Loads an entire Keras model from a file.
 
+    Args:
+        filename: The path of the file from which the model should be loaded.
 
-# --- TEST HİSSƏSİ (Yoxlamaq üçün) ---
-if __name__ == '__main__':
-    # Sadə bir test modeli yaradırıq
-    inputs = tf.keras.Input(shape=(10,))
-    outputs = tf.keras.layers.Dense(3)(inputs)
-    model = tf.keras.Model(inputs=inputs, outputs=outputs)
-
-    # Test datası ilə yoxlayırıq
-    data = tf.random.normal((1, 10))
-    print("Orijinal modelin nəticəsi:")
-    print(model(data).numpy())
-
-    # Modeli yadda saxlayırıq
-    save_model(model, 'network.keras')
-    print("\nModel 'network.keras' olaraq yadda saxlanıldı.")
-
-    # Modeli yenidən yükləyirik
-    loaded_net = load_model('network.keras')
-    print("\nModel fayldan geri yükləndi.")
-    
-    print("\nYüklənmiş modelin nəticəsi (eyni olmalıdır):")
-    print(loaded_net(data).numpy())
+    Returns:
+        The loaded Keras model.
+    """
+    return K.models.load_model(filename)
