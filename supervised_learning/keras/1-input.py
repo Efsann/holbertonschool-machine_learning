07099f780cdb8000df5dd26e48df5ce96da4894c
efsann <stiyevefsan@gmail.com>
2026-07-17 19:53:00 +0000

    Fix pycodestyle completely for 1-input.py

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
 """
-Module to build a model using Keras Functional API
+Module containing function to build a model with Keras input
 """
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-    Builds a neural network with the Keras library using Functional API.
+    Builds a neural network with Keras layers.
 
-    nx: number of input features
-    layers: list containing the number of nodes in each layer
+    Parameters:
+    nx: number of input features to the network
+    layers: list containing the number of nodes in each layer of the network
     activations: list containing the activation functions for each layer
     lambtha: L2 regularization parameter
     keep_prob: probability that a node will be kept for dropout
+
+    Returns:
+    The keras model
     """
     inputs = K.Input(shape=(nx,))
-    regularizer = K.regularizers.L2(lambtha)
     x = inputs
 
     for i in range(len(layers)):
         x = K.layers.Dense(
-            units=layers[i],
+            layers[i],
             activation=activations[i],
-            kernel_regularizer=regularizer
+            kernel_regularizer=K.regularizers.l2(lambtha)
         )(x)
 
-        if i < len(layers) - 1:
+        if i < len(layers) - 1 and keep_prob is not None:
             x = K.layers.Dropout(1 - keep_prob)(x)
 
     model = K.Model(inputs=inputs, outputs=x)
     return model
-
