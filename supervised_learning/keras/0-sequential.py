71262c85aabd687d4789144352e1ffcdbed91152
efsann <stiyevefsan@gmail.com>
2026-07-17 19:24:35 +0000

    Fix: Added README.md and optimized model imports

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,36 +1,33 @@
 #!/usr/bin/env python3
 """
-Mövzu: Keras ilə Sequential neyron şəbəkəsinin qurulması
+Module to build a sequential model using Keras
 """
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-    Keras kitabxanası istifadə edərək neyron şəbəkəsi qurur.
-    
-    nx: şəbəkənin giriş xüsusiyyətlərinin sayı
-    layers: hər təbəqədəki düyünlərin sayı (siyahı)
-    activations: hər təbəqənin aktivasiya funksiyası (siyahı)
-    lambtha: L2 requlyarizasiya parametri
-    keep_prob: Dropout üçün düyünün saxlanılma ehtimalı
+    Builds a neural network with the Keras library
     """
     model = K.Sequential()
     regularizer = K.regularizers.L2(lambtha)
-    
+
     for i in range(len(layers)):
         if i == 0:
-            model.add(K.layers.Dense(units=layers[i],
-                                     activation=activations[i],
-                                     kernel_regularizer=regularizer,
-                                     input_shape=(nx,)))
+            model.add(K.layers.Dense(
+                units=layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer,
+                input_shape=(nx,)
+            ))
         else:
-            model.add(K.layers.Dense(units=layers[i],
-                                     activation=activations[i],
-                                     kernel_regularizer=regularizer))
-        
+            model.add(K.layers.Dense(
+                units=layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer
+            ))
+
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
-            
-    return model
 
+    return model
---
+++ supervised_learning/keras/README.md
@@ -0,0 +1,2 @@
+# Keras Supervised Learning
+This directory contains tasks for understanding and building neural networks using the Keras library.
