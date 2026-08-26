113ec563021768c104e814a977367166073230e2
efsann <stiyevefsan@gmail.com>
2026-08-26 11:16:36 +0000

    Implement learning_rate_decay operation in TensorFlow

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module to create a learning rate decay operation in TensorFlow
+"""
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """
+    Creates a learning rate decay operation in TensorFlow using inverse
+    time decay in a stepwise fashion.
+
+    Parameters:
+    alpha: original learning rate
+    decay_rate: weight used to determine the decay rate
+    decay_step: number of passes before alpha decays further
+
+    Returns:
+    The learning rate decay operation
+    """
+    return tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True
+    )
