c46efdacd8816b0dc0ed1bfa7eb8eaead4b99b9d
efsann <stiyevefsan@gmail.com>
2026-08-30 16:36:59 +0000

    Add forward_prop method to NeuralNetwork class

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""
+Module defining a neural network with forward propagation
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Class NeuralNetwork that defines a neural network with one hidden layer
+    performing binary classification
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        Class constructor for NeuralNetwork
+
+        Parameters:
+        - nx: int, number of input features
+        - nodes: int, number of nodes found in the hidden layer
+
+        Raises:
+        - TypeError: if nx or nodes is not an integer
+        - ValueError: if nx or nodes is less than 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for __W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for __b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for __A1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for __W2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for __b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for __A2"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neural network
+
+        Parameters:
+        - X: numpy.ndarray with shape (nx, m) containing input data
+
+        Returns:
+        - Private attributes __A1 and __A2, respectively
+        """
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
