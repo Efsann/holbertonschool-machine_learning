85fd04526709d4d51eddc8c2b50fe98130f8a29f
efsann <stiyevefsan@gmail.com>
2026-08-30 16:36:03 +0000

    Create NeuralNetwork class with constructor

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Module defining a neural network with one hidden layer
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Class NeuralNetwork that defines a neural network with one hidden layer
+    performing binary classification
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        Class constructor for NeuralNetwork
+
+        Parameters:
+        - nx: int, number of input features
+        - nodes: int, number of nodes found in the hidden layer
+
+        Raises:
+        - TypeError: if nx or nodes is not an integer
+        - ValueError: if nx or nodes is less than 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
